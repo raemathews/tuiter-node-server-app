238e77dfcc9295f2149a0c0df1ca716c2e6f8fcf
Raven Mathews <raegmathew@gmail.com>
2023-04-02 21:55:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import HelloController
+  from "./controllers/hello-controller.js"
+import UserController
+  from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors'
+import express from 'express';
+const app = express()
+app.use(cors);
+app.use(express.json());
+HelloController(app);
+UserController(app);
+TuitsController(app)
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(process.env.PORT || 4000);
