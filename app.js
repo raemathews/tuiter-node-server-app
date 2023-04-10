cf9325395c7c335cc88509c67325306010945709
Raven Mathews <raegmathew@gmail.com>
2023-04-09 22:01:38 -0400

    a9

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import UserController
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
 import express from 'express';
+import mongoose from "mongoose";
 const app = express()
 app.use(cors());
 app.use(express.json());
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(DB_CONNECTION_STRING);
 HelloController(app);
 UserController(app);
 TuitsController(app)
